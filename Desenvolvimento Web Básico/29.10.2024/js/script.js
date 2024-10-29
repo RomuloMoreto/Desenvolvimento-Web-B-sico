57e5cff626c9817e8b6bc3c934c956dc3a4ab931
Rômulo Moreto <romulomoreto09@gmail.com>
2024-10-29 11:31:56 -0300

    Add files via upload

---
+++ Desenvolvimento Web Básico/29.10.2024/js/script.js
@@ -0,0 +1,13 @@
+function escreveConsole(){
+    console.log('Viu a mensagem');
+}
+
+function exibeDados(){
+    var nome = document.getElementById('nome').value;
+    var senha = document.getElementById('senha').value;
+    document.getElementById('result').innerHTML = "Nome: "+ nome +" Senha: "+senha;
+}
+
+function verRadio(){
+
+}
